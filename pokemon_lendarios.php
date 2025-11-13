6c7b99ab506dd485f60b64ccd2f9beb006b38d59
Rafael Bressan Fiorio <138718898+Rfzz1@users.noreply.github.com>
2025-11-13 16:55:52 -0300

    Página dos pokemon lendarios + css

--- pokemon_lendarios.php
+++ pokemon_lendarios.php
@@ -11,9 +11,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Chelsea+Market&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/national_dex.css">
     <link rel="stylesheet" href="css/busca_national.css">
+	<link rel="stylesheet" href="css/plendario.css">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,12 +22,23 @@
 
 <body>
 
-<?php include 'conexao.php'; include 'cabecalho.php';?>
+<?php include 'conexao.php'; include 'cabecalho.php';?> 
+<h1 id="titulo">Pokémon Lendários</h1>
 
-<!------ CONTAINER TÍTULO + BUSCA ------>
-    <div class="header-container">
-        <h1 id="titulo">Pokémon Lendários</h1>
-    </div>
+<div class="container">
+	<p class="paragrafo">São um grupo de Pokémon extremamente raros e frequentemente muito poderosos, geralmente protagonizando lendas e mitos no mundo pokémon.
+	Muitos lendários podem ser considerados divindades dentro da espécie Pokémon, e alguns desempenham papéis fundamentais na criação e 
+	funcionamento do Mundo Pokémon. Podem assumir formas alternativas e a sua maioria possui gênero indefinido, além de não poderem se reproduzir ou chocarem ovos. Com exceção  de Type: Null, Cosmog e Kubfu, os Pokémon lendários também são conhecidos por não evoluírem.
+	identificá-los como pertencentes desse grupo é através de mídias oficiais, como o anime ou os jogos. Uma menção honrosa ao Arcanine é preciso ser feita, afinal, tanto o anime quanto as Dex Entries dele em alguns jogos confirmam que esse Pokémon é considerado um Lendário, apesar de ser encontrado com facilidade na natureza.</p>
+	<img src="img/lendarios.jpg" class="img_lendarios">
+</div>
 
+<h2 class="subtitulo" id="disponibilidade">Disponibilidade</h2>
+
+<div class="container">
+	<p class="paragrafo" id="disponibilidade-text">A disponibilidade dos lendários varia conforme a mídia. Nos jogos e mangá, geralmente existe apenas um exemplar de cada lendário, com raras exceções como Heatran, Type: Null e Kubfu. Casos de múltiplas capturas ou batalhas especiais ocorrem apenas por motivos de gameplay. O mangá destaca que manter esses Pokémon em Poké Bolas por muito tempo pode desequilibrar o mundo, embora versões de universos paralelos possam existir, como o Celebi shiny de Mystery Dungeon, que é fêmea, diferente do Celebi original.
+	Já o anime adota uma lógica mais flexível, permitindo múltiplos exemplares e até reprodução entre lendários, como Lugia e Latios. Essa ideia já aparecia em Pokémon Snap, que mostra ovos de Articuno, Zapdos e Moltres.</p>
+	<img src="img/lugia.jpg" class="img_lendarios" id="lugia">
+</div>
 </body>
 </html>
